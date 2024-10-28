66ffa89be63763009579772633ca9df9b29c0551
birkurwar jay <birkurwarjay01@gmail.com>
2024-10-28 17:30:05 +0530

    i f

---
+++ ThemeToggler/src/Contexts/Theme.js
@@ -0,0 +1,13 @@
+import react, {createContext, useContext} from "react";
+
+export const themeContext = createContext({
+    themeMode: "light",
+    lightTheme: () => { },
+    darkTheme: () => { }
+});
+
+export const ThemeProvider = themeContext.Provider;
+
+export default function useTheme() {
+    return useContext(themeContext);
+}
